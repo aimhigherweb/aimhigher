f9c0232371388520e89346a565cba5855ed6dc50
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-09-28 00:17:19 +0800

    kinda got meta tags working but need to work on it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import DocumentMeta from 'react-document-meta';
+
 
 //Resources
 import './resources/content.css';
@@ -20,36 +22,92 @@ export const menuItems = [
       slug: '/',
       title: 'Home',
       component: () => <Home />,
+      meta: {
+        title: 'AimHigher Web Design',
+        description: 'AimHigher Web Design bridges the gap between technology and business throughout Australia.',
+        canonical: 'https://aimhigherwebdesign.com.au',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/about-us',
       title: 'About Us',
       component: () => <About />,
+      meta: {
+        title: 'About Us | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/about-us',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/products-services',
       title: 'Products and Services',
       component: () => <ProductsServices />,
+      meta: {
+        title: 'Products & Services | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/product-services',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/portfolio',
       title: 'Portfolio',
       component: () => <Portfolio />,
+      meta: {
+        title: 'Portfolio | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/portfolio',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/faq',
       title: 'FAQ',
       component: () => <Faq />,
+      meta: {
+        title: 'FAQ | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/faq',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/code-of-ethics',
       title: 'Code of Ethics',
       component: () => <CodeEthics />,
+      meta: {
+        title: 'Code of Ethics | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/code-of-ethics',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
   {
       slug: '/contact',
       title: 'Contact',
       component: () => <Contact />,
+      meta: {
+        title: 'Contact | AimHigher Web Design',
+        description: '',
+        canonical: 'https://aimhigherwebdesign.com.au/contact',
+        meta: {
+          charset: 'utf-8',
+        }
+      }
   },
 ];
 
@@ -57,7 +115,10 @@ export class App extends Component {
   render() {
     let pages = menuItems.map(page => {
       return(
-        <Route exact path={page.slug} component={page.component} />
+        <div>
+          <DocumentMeta {...page.meta} extend />
+          <Route exact path={page.slug} component={page.component} />
+        </div>
       );
     });
 
