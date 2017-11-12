5c7e90c48c365ed246afe7473913c830d461d474
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-11-12 19:49:36 +0800

    added typeform to contact page

--- src/layouts/contact.js
+++ src/layouts/contact.js
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
 import {Helmet} from 'react-helmet';
+import {ReactTypeformEmbed} from 'react-typeform-embed';
 
 //Resources
 // import './resources/home.css';
 
+class Meta extends Component {
+    render() {
+        let name = 'Contact Us | AimHigher Web Design';
+        let description ="Get in touch with us today and find out how we can help you";
+        let slug = 'contact';
+        let image = 'https://aimhigherwebdesign.com.au/img/logo.png';
+        return (
+            <Helmet>
+                <title>{name}</title>
+                <meta name="description" content={description} />
+                <link rel="canonical" href={'https://aimhigherwebdesign.com.au/' + slug} />
+
+                {/* Facebook */}
+                <meta property="og:url" content={'https://aimhigherwebdesign.com.au/' + slug} />
+                
+                <meta property="og:title" content={name} />
+                <meta property="og:image" content={image} />
+                <meta property="og:description" content={description} />
+
+                {/* Twitter */}
+                <meta name="twitter:url" content={'https://aimhigherwebdesign.com.au/' + slug} />
+                <meta name="twitter:title" content={name} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:image" content={image} />
+            </Helmet>
+        );
+    }
+};
+
 export class Contact extends Component {
     render() {
         return (
-            <div>
-                <Helmet>
-                    <title> Contact Us | AimHigher Web Design</title>
-                    <meta name="description" content="Contact us today to see how we can help your business" />
-                    <link rel="canonical" href="https://aimhigherwebdesign.com.au/contact" />
-
-                    {/* Facebook */}
-                    <meta property="og:url" content="https://aimhigherwebdesign.com.au/contact" />
-                    
-                    <meta property="og:title" content="Contact Us | AimHigher Web Design" />
-                    <meta property="og:image" content="https://aimhigherwebdesign.com.au/img/logo.png" />
-                    <meta property="og:description" content="Contact us today to see how we can help your business" />
-
-                    {/* Twitter */}
-                    <meta name="twitter:url" content='https://aimhigherwebdesign.com.au/contact' />
-                    <meta name="twitter:title" content="Contact Us | AimHigher Web Design" />
-                    <meta name="twitter:description" content="Contact us today to see how we can help your business" />
-                    <meta name="twitter:image" content="https://aimhigherwebdesign.com.au/img/logo.png" />
-                </Helmet>
-                <h1>Contact Us</h1>
-                <h2>Hello World</h2>
+            <div className="content">
+                <Meta />
+                <ReactTypeformEmbed url={'https://aimhigherwebdesign.typeform.com/to/LeTkEc'} />
             </div>
         );
     }
