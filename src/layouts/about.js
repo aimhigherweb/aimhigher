64c1308fc2abd6eb4e56886dd1133ee0c3e5d456
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-11-16 15:15:30 +0800

    did about us page

--- src/layouts/about.js
+++ src/layouts/about.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 //Resources
-// import './resources/faq.css';
+import '../resources/about.css';
 
 export class About extends Component {
     render() {
         return (
-            <div>
+            <div className="content">
                 <h1>About</h1>
-                <h2>Hello World</h2>
+                <div className="wrap">
+                    <div>
+                        <p>
+                            I grew up in country Queensland and most people we knew had small businesses. But when I found out how much it cost them to get a website built, I couldn't believe it.
+                        </p>
+                        <p>
+                            AimHigher Web Design was founded to help bridge the gap between small businesses and technology, and help everyone (no matter what size your business is, or if you're a sole trader), get their business online.
+                        </p>
+                        <p>
+                            We specialise in helping you out with the entire process, starting the website, setting up social media, setting up emails and any of the ongoing support and maintenance requirements.
+                        </p>
+                        <p>
+                            You know your business the best, and we know the web. Let us help you, and leave you to focus on what you know best!
+                        </p>
+                    </div>
+                    <div className="image-container">
+                        <img alt="Profile Image of Amy" src="/img/profile.jpg" />
+                    </div>
+                </div>
             </div>
         );
     }
