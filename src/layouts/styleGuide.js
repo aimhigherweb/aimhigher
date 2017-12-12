754e3064056ddad64f3be000a6cd862f0fea4381
Amy Kapernick <amy@aimhigherwebdesign.com>
2017-12-12 18:34:19 +0800

    style guide working

--- src/layouts/styleGuide.js
+++ src/layouts/styleGuide.js
@@ -62,11 +62,21 @@ class ColourSwatches extends Component {
         };
 
         for (var key in colours) {
-            const hexToRgb = hex =>
-            hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-                       ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-              .substring(1).match(/.{2}/g)
-              .map(x => parseInt(x, 16))
+            const hexToRgb = function(str) { 
+                if ( /^#([0-9a-f]{3}|[0-9a-f]{6})$/ig.test(str) ) { 
+                    var hex = str.substr(1);
+                    hex = hex.length == 3 ? hex.replace(/(.)/g, '$1$1') : hex;
+                    var rgb = parseInt(hex, 16);               
+                    return [(rgb >> 16) & 255, (rgb >> 8) & 255, rgb & 255];
+                } 
+            
+                return false; 
+            }
+
+            const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+                const hex = x.toString(16)
+                return hex.length === 1 ? '0' + hex : hex
+            }).join('');
           
             let colourMain = hexToRgb(colours[key]);
 
@@ -85,44 +95,169 @@ class ColourSwatches extends Component {
                 };
 
                 for(let i = 0; i < 3; i++) {
-                    r[i] = Math.round(colour + ((1 - opacity) * rgb[i]));
+                    r[i] = Math.round((opacity * rgb[i]) + ((1 - opacity) * colour));
                 };
 
                 return r;
             };
 
-            // let light_90 = convert(colourMain, 0.9, 'light');
-
             colours[key] = {
-                main: colourMain,
-                light_90: convert(colourMain, 0.9, 'light'),
+                main: {
+                    name: 'main',
+                    rgb: colourMain,
+                },
+                light: {
+                    90: {
+                        name: '90',
+                        rgb: convert(colourMain, 0.9, 'light')
+                    },
+                    75: {
+                        name: '75',
+                        rgb: convert(colourMain, 0.75, 'light'),
+                    },
+                    50: {
+                        name: '50',
+                        rgb: convert(colourMain, 0.5, 'light'),
+                    },
+                    25: {
+                        name: '25',
+                        rgb: convert(colourMain, 0.25, 'light'),
+                    },
+                    10: {
+                        name: '10',
+                        rgb: convert(colourMain, 0.1, 'light'),
+                    },
+                },
+                dark: {
+                    90: {
+                        name: '90',
+                        rgb: convert(colourMain, 0.9, 'dark'),
+                    },
+                    75: {
+                        name: '75',
+                        rgb: convert(colourMain, 0.75, 'dark'),
+                    },
+                    50: {
+                        name: '50',
+                        rgb: convert(colourMain, 0.5, 'dark'),
+                    },
+                    25: {
+                        name: '25',
+                        rgb: convert(colourMain, 0.25, 'dark'),
+                    },
+                    10: {
+                        name: '10',
+                        rgb: convert(colourMain, 0.1, 'dark'),
+                    },
+                },   
             };
 
-        };
+            for (var cols in colours[key]) {
+                if(colours[key][cols] == colours[key].main) {
+                    let rgbVal = [
+                        colours[key][cols].rgb[0], 
+                        colours[key][cols].rgb[1], 
+                        colours[key][cols].rgb[2],
+                    ];
 
+                    colours[key][cols].hex = rgbToHex(rgbVal[0], rgbVal[1], rgbVal[2]);
+                    
+                    colours[key][cols].variant = (rgbVal[0] * 0.299) + (rgbVal[0] * 0.587) + (rgbVal[0] * 0.114);
+                };
+
+                for (var opts in colours[key][cols]) {
+                    if(colours[key][cols] !== colours[key].main) {
+                        let rgbVal = [
+                            colours[key][cols][opts].rgb[0], 
+                            colours[key][cols][opts].rgb[1], 
+                            colours[key][cols][opts].rgb[2],
+                        ];
+        
+                        colours[key][cols][opts].hex = rgbToHex(rgbVal[0], rgbVal[1], rgbVal[2]);
+        
+                        colours[key][cols][opts].variant = (rgbVal[0] * 0.299) + (rgbVal[0] * 0.587) + (rgbVal[0] * 0.114);
+                    };
+                };
+            };
+        };
         this.setState({colours: colours});
     };
 
     render() {
-        console.log(this.state.colours);
+        let colours = this.state.colours;
+
+        let colArray = [colours.primary, colours.supporting, colours.neutral];
+
+        let colSets = colArray.map(set => {
+            return (
+                <ColourSet set={set} />
+            );
+        });
+
+        return (
+            <div>
+                {colSets}
+            </div>
+        );
+    };
+};
+
+class ColourSwatch extends Component {
+    render() {
+        let thisCols = this.props.cols;
+        let thisHex = thisCols.hex;
+        let vars = thisCols.variant;
+        let opts = thisCols.name;
+        let type;
+
+        if(vars >= 186) {
+            type = 'light';
+        }
+        else {
+            type = 'dark';
+        };
+
+        return (
+            <div className={'colour swatch ' + opts + ' ' + type} style={{background: thisHex}}>
+                <p>{thisHex}</p>
+            </div>
+        );
+    };
+};
+
+class ColourGroup extends Component {
+    render() {
+        let colArray = [];
+
+        for (var opts in this.props.colgroup) {
+            colArray.unshift(this.props.colgroup[opts]);
+        };
+
+        let colSwatches = colArray.map(swatch => {
+            return (
+                <ColourSwatch key={swatch.name} cols={swatch} />
+            );
+        });
+
+        return (
+            <div className={'colour group ' + this.props.vars}>
+                {colSwatches}
+            </div>
+        );
+    };
+};
+
+class ColourSet extends Component {
+    render() {
+        let colGroupLight = this.props.set.light;
+        let colGroupDark = this.props.set.dark;
+        let colMain = this.props.set.main;
 
         return (
             <div className="colour set">
-                <div className="colour swatch main" style={{background: this.state.colours.primary.main}} >{this.state.colours.primary.main}</div>
-                <div className="colour group light">
-                    <div className="colour swatch 90"></div>
-                    <div className="colour swatch 75"></div>
-                    <div className="colour swatch 50"></div>
-                    <div className="colour swatch 25"></div>
-                    <div className="colour swatch 10"></div>
-                </div>
-                <div className="colour group dark">
-                    <div className="colour swatch 90"></div>
-                    <div className="colour swatch 75"></div>
-                    <div className="colour swatch 50"></div>
-                    <div className="colour swatch 25"></div>
-                    <div className="colour swatch 10"></div>
-                </div>
+                <ColourSwatch cols={colMain} />
+                <ColourGroup colgroup={colGroupLight} vars='light' />
+                <ColourGroup colgroup={colGroupDark} vars='dark' />
             </div>
         );
     };
