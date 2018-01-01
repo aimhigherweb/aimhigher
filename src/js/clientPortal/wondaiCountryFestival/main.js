39a4bca152c3367af97224b5eeb505140a3153c0
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-01-02 06:45:58 +0800

    transitioned over to use my webpack setup, still need to fix images

---
+++ src/js/clientPortal/wondaiCountryFestival/main.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
+// Components
+import { StyleGuide } from './styleGuide';
+
+//Resources
+import '../../../scss/clientPortal/wondaiCountryFestival/styles.scss';
+
+class Meta extends Component {
+    render() {
+        let name = 'Wondai Country Running Festival - Client Information';
+        let description ="Don't look through emails, it's all here";
+        let slug = 'wondai-country-festival';
+        let image = 'https://aimhigherwebdesign.com.au/img/logo.png';
+        return (
+            <Helmet>
+                <title>{name}</title>
+                <meta name="description" content={description} />
+                <link rel="canonical" href={'https://aimhigherwebdesign.com.au/' + slug} />
+
+                {/* Facebook */}
+                <meta property="og:url" content={'https://aimhigherwebdesign.com.au/' + slug} />
+                
+                <meta property="og:title" content={name} />
+                <meta property="og:image" content={image} />
+                <meta property="og:description" content={description} />
+
+                {/* Twitter */}
+                <meta name="twitter:url" content={'https://aimhigherwebdesign.com.au/' + slug} />
+                <meta name="twitter:title" content={name} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:image" content={image} />
+            </Helmet>
+        );
+    }
+};
+
+export class WondaiCountry extends Component {
+    render() {
+        document.getElementById('root').classList.add('style-guide');
+
+        return (
+            <div className="content typography">
+                <Meta />
+                <h1>Wondai Country Running Festival</h1>
+                <div>
+                    <StyleGuide />
+                </div>
+            </div>
+        );
+    }
+};
---
+++ src/js/partial/app.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+
+//Resources
+import '../../scss/content.scss';
+
+//Components
+import {Home} from '../layouts/home.js';
+import {About} from '../layouts/about.js';
+import {ProductsServices} from '../layouts/products.js';
+import {Portfolio} from '../layouts/portfolio.js';
+import {Faq} from '../layouts/faq.js';
+import {CodeEthics} from '../layouts/ethics.js';
+import {Contact} from '../layouts/contact.js';
+import {Terms} from '../layouts/terms.js';
+import {Privacy} from '../layouts/privacy.js';
+import {StyleGuide} from '../layouts/styleGuide.js';
+
+//Client Pages
+import {WondaiCountry} from '../clientPortal/wondaiCountryFestival/main.js';
+import {SocialPilot} from '../clientPortal/wondaiCountryFestival/socialpilot.js';
+import {GlenrockHay} from '../clientPortal/glenrockHay/main.js';
+
+//Variables
+export const menuItems = [
+  {
+    slug: '/',
+    title: 'Home',
+    component: () => <Home />,
+  },
+  {
+    slug: '/about-us',
+    title: 'About Us',
+    component: () => <About />,
+  },
+  {
+    slug: '/services',
+    title: 'Services',
+    component: () => <ProductsServices />,
+  },
+  {
+    slug: '/portfolio',
+    title: 'Portfolio',
+    component: () => <Portfolio />,
+  },
+  {
+    slug: '/faq',
+    title: 'FAQ',
+    component: () => <Faq />,
+  },
+  {
+    slug: '/code-of-ethics',
+    title: 'Code of Ethics',
+    component: () => <CodeEthics />,
+  },
+  {
+    slug: '/contact',
+    title: 'Contact',
+    component: () => <Contact />,
+  },
+];
+
+export const legalItems = [
+  {
+    slug: '/privacy',
+    title: 'Privacy Policy',
+    component: () => <Privacy />,
+  },
+  {
+    slug: '/terms',
+    title: 'Terms of Use',
+    component: () => <Terms />,
+  },
+]
+
+const hiddenPages = [
+  {
+    slug: '/style-guide',
+    title: 'Style Guide',
+    component: () => <StyleGuide />,
+  },
+]
+
+const clientPages = [
+  {
+    slug: '/wondai-country-festival',
+    title: 'Wondai Country Running Festival',
+    component: () => <WondaiCountry />,
+  },
+  {
+    slug: '/wondai-country-festival/socialpilot',
+    title: 'SocialPilot Instructions - Wondai Country Running Festival',
+    component: () => <SocialPilot />,
+  },
+  {
+    slug: '/glenrock-hay',
+    title: 'Glenrock Hay',
+    component: () => <GlenrockHay />,
+  },
+]
+
+const routeItems = menuItems.concat(legalItems).concat(hiddenPages).concat(clientPages);
+
+export class App extends Component {
+  render() {
+    window.onscroll = function() {
+      const perHeight = window.innerHeight * 0.3;
+      if(document.documentElement.scrollTop > perHeight) {
+          document.getElementById('root').classList.add('scrolled');
+      }
+      else {
+          document.getElementById('root').classList.remove('scrolled');
+      };
+    };
+
+    document.getElementById('root').classList.remove('style-guide');
+
+    let pages = routeItems.map(page => {
+      return(
+        <div key={page.title}>
+          <Route exact path={page.slug} component={page.component} key={page.slug} />
+        </div>
+      );
+    });
+
+    return (
+      <Router>
+        <div className="container">
+          {pages}
+        </div>
+      </Router>
+    );
+  }
+}
