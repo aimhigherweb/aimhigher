4497d1ea98b97c3017b7e719e6fbf7414418c438
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-01-07 18:57:02 +0800

    optimising stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {render} from 'react-snapshot';
+import render from 'react-snapshot';
+import {ReactDOM} from 'react-dom';
 
 //Import components
 import {Header} from './partial/header.js';
--- webpack.common.js
+++ webpack.common.js
@@ -2,22 +2,12 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractSass = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "development"
-});
-
 module.exports = {
   entry: ["./src/js/index.js"],
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "js/[name].js"
   },
-  devServer: {
-    contentBase: "./dist",
-    historyApiFallback: true
-  },
-  devtool: "source-map", // any "source-map"-like devtool is possible
   module: {
     rules: [
       {
