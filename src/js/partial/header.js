23d0e0c726a6f1ffeaa21e84ff8d2d2b6354faba
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-01-07 21:12:37 +1100

    Change setup (#15)
    * transitioned over to use my webpack setup, still need to fix images
    
    * updated images (except portfolio and products cos I've got to work out what I'm doing there
    
    * updated where I'm storing data
    
    * pushing
    
    * fixed the thing with crossing styleguide colours
    
    * removed the dark/light colour swatches when the colour is black/white
    
    * fixed the deal with lots of images
    
    * just the meta deets and favicon to go
    
    * added fragments, because :fire:
    
    * updated readme
    
    * made it copy the 404 file
    
    * added react snapshot
    
    * trying something

---
+++ src/js/partial/header.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {BrowserRouter as Router, NavLink} from 'react-router-dom';
+import ReactSVG from 'react-svg';
+
+//Components
+import {App, menuItems} from './app.js';
+
+//Resources
+import Logo from '../../img/logo.svg';
+import {Menu} from 'react-feather';
+import '../../scss/partials/header.scss';
+
+export class Header extends React.Component {
+    render() {
+        return (
+          <div className="inner">
+            <SiteTitle />
+            <MainMenu />
+          </div>
+        );
+    };
+};
+
+const SiteTitle = (logo => (
+    <div className="site-logo">
+        <a href="/">
+            <ReactSVG path={Logo} />
+        </a>
+    </div>
+));
+
+class MainMenu extends React.Component {
+    mobileMenu() {
+        if(document.getElementsByClassName('main menu active').length < 1) {
+            document.getElementById('nav-main').classList.add('active');
+        }
+        else {
+            document.getElementById('nav-main').classList.remove('active');
+        };
+    };
+    
+
+    render() {
+        let navItems = menuItems.map(navItem => {
+            return(
+                <li key={navItem.title}>
+                    <NavLink to={navItem.slug} onClick={<App />} activeClassName="current">
+                        {navItem.title}
+                    </NavLink>
+                </li>
+            );
+        });
+
+        return (
+            <nav id="nav-main" className="menu main">
+                <a href="#" className="hamburger" onClick={this.mobileMenu}>
+                    <Menu />
+                </a>
+                <Router>
+                    <ul>
+                        {navItems}
+                    </ul>
+                </Router>
+            </nav>
+        );
+    };
+};
