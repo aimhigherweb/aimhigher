93e74aaf5bf37b6a091311826471273326a0b2ce
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-01-22 20:51:23 +0800

    currently getting babel-runtime error on build

--- src/index.js
+++ src/index.js
@@ -3,35 +3,17 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 //Import components
-// import {Header} from './js/partial/header.js';
-// import {App} from './js/partial/app.js';
-// import {Footer} from './js/partial/footer.js';
+import {Header} from './js/partial/header.js';
+import {App} from './js/partial/app.js';
+import {Footer} from './js/partial/footer.js';
 
 //Resources
-// import './scss/global.scss';
+import './scss/global.scss';
 
-// ReactDOM.render(<Header />, document.getElementById('header'));
-
-class App extends Component {
-    render() {
-  
-      return (
-        <h1>Hello World!</h1>
-      );
-    }
-};
+ReactDOM.render(<Header />, document.getElementById('header'));
 
 ReactDOM.render(<App />, document.getElementById('main'));
 
-// ReactDOM.render(<Footer />, document.getElementById('footer'));
+ReactDOM.render(<Footer />, document.getElementById('footer'));
 
-// if (module.hot) {
-//     module.hot.dispose(function () {
-//         //Module is about to be replaced
-//     });
-
-//     module.hot.accept(function () {
-//         //module or one of it's dependencies was just updated
-//     });
-// };
 
