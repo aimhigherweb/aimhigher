c6d7c1d004e9abc417ca24db3929d009bf6ad03d
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-02-03 23:35:49 +0800

    Change build setup to use parcel.js (#18)
    * trying out parcel.js setup
    
    * most;y done, but it's broken
    
    * currently getting babel-runtime error on build
    
    * Switched to Parcel.js instead of webpack
    
    * working on bulk import of images
    
    * WHOOgit add -A! IT'S WORKINGgit add -Agit add -A :sparkles: :tada: :boom:
    
    * WHOO IT'S WORKING :sparkles: :tada: :boom:
    
    * changed the build script, I think that fixes it
    
    * modified the build process to use index.html and make a 404 file
    
    * fixed asset file paths
    
    * tweaked a few config things and updated a few funky things
    
    * updated a couple of accessibility changes

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	jsxBracketsSameLine: true,
+	printWidth: 80,
+	singleQuote: true,
+	semi: true,
+	tabWidth: 4,
+	trailingComma: 'all',
+	useTabs: true,
+};
--- src/js/partial/app.js
+++ src/js/partial/app.js
@@ -1,166 +1,182 @@
-import React, {Component, Fragment} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Helmet from 'react-helmet';
-
 
 //Resources
 import '../../scss/content.scss';
 
 //Components
-import {Home} from '../layouts/home.js';
-import {About} from '../layouts/about.js';
-import {ProductsServices} from '../layouts/products.js';
-import {Portfolio} from '../layouts/portfolio.js';
-import {Faq} from '../layouts/faq.js';
-import {CodeEthics} from '../layouts/ethics.js';
-import {Contact} from '../layouts/contact.js';
-import {Terms} from '../layouts/terms.js';
-import {Privacy} from '../layouts/privacy.js';
-import {StyleGuide} from '../layouts/styleGuide.js';
+import { Home } from '../layouts/home.js';
+import { About } from '../layouts/about.js';
+import { ProductsServices } from '../layouts/products.js';
+import { Portfolio } from '../layouts/portfolio.js';
+import { Faq } from '../layouts/faq.js';
+import { CodeEthics } from '../layouts/ethics.js';
+import { Contact } from '../layouts/contact.js';
+import { Terms } from '../layouts/terms.js';
+import { Privacy } from '../layouts/privacy.js';
+import { StyleGuide } from '../layouts/styleGuide.js';
 
 //Client Pages
-import {WondaiCountry} from '../clientPortal/wondaiCountryFestival/main.js';
-import {SocialPilot} from '../clientPortal/wondaiCountryFestival/socialpilot.js';
-import {GlenrockHay} from '../clientPortal/glenrockHay/main.js';
-
-//Importing Images
-var images = {};
-
-function importAll (r) {
-    r.keys().forEach(key => images[key] = r(key));
-}
-
-importAll(require.context('../../img/favicons/', true, /\.png$/));
+import { WondaiCountry } from '../clientPortal/wondaiCountryFestival/main.js';
+import { SocialPilot } from '../clientPortal/wondaiCountryFestival/socialpilot.js';
+import { GlenrockHay } from '../clientPortal/glenrockHay/main.js';
 
 // Variables
 export const menuItems = [
-  {
-    slug: '/',
-    title: 'Home',
-    component: () => <Home />,
-  },
-  {
-    slug: '/about-us',
-    title: 'About Us',
-    component: () => <About />,
-  },
-  {
-    slug: '/services',
-    title: 'Services',
-    component: () => <ProductsServices />,
-  },
-  {
-    slug: '/portfolio',
-    title: 'Portfolio',
-    component: () => <Portfolio />,
-  },
-  {
-    slug: '/faq',
-    title: 'FAQ',
-    component: () => <Faq />,
-  },
-  {
-    slug: '/code-of-ethics',
-    title: 'Code of Ethics',
-    component: () => <CodeEthics />,
-  },
-  {
-    slug: '/contact',
-    title: 'Contact',
-    component: () => <Contact />,
-  },
-];
-  
-export const legalItems = [
-  {
-    slug: '/privacy',
-    title: 'Privacy Policy',
-    component: () => <Privacy />,
-  },
-  {
-    slug: '/terms',
-    title: 'Terms of Use',
-    component: () => <Terms />,
-  },
-]
-  
-const hiddenPages = [
-  {
-    slug: '/style-guide',
-    title: 'Style Guide',
-    component: () => <StyleGuide />,
-  },
-];
-  
-const clientPages = [
-  {
-    slug: '/wondai-country-festival',
-    title: 'Wondai Country Running Festival',
-    component: () => <WondaiCountry />,
-  },
-  {
-    slug: '/wondai-country-festival/socialpilot',
-    title: 'SocialPilot Instructions - Wondai Country Running Festival',
-    component: () => <SocialPilot />,
-  },
-  {
-    slug: '/glenrock-hay',
-    title: 'Glenrock Hay',
-    component: () => <GlenrockHay />,
-  },
+	{
+		slug: '/',
+		title: 'Home',
+		component: () => <Home />,
+		class: 'hidden',
+	},
+	{
+		slug: '/about-us',
+		title: 'About Us',
+		component: () => <About />,
+	},
+	{
+		slug: '/services',
+		title: 'Services',
+		component: () => <ProductsServices />,
+	},
+	{
+		slug: '/portfolio',
+		title: 'Portfolio',
+		component: () => <Portfolio />,
+	},
+	{
+		slug: '/faq',
+		title: 'FAQ',
+		component: () => <Faq />,
+	},
+	{
+		slug: '/code-of-ethics',
+		title: 'Code of Ethics',
+		component: () => <CodeEthics />,
+	},
+	{
+		slug: '/contact',
+		title: 'Contact',
+		component: () => <Contact />,
+	},
 ];
 
-const routeItems = menuItems.concat(legalItems).concat(hiddenPages).concat(clientPages);
+export const legalItems = [
+	{
+		slug: '/privacy',
+		title: 'Privacy Policy',
+		component: () => <Privacy />,
+	},
+	{
+		slug: '/terms',
+		title: 'Terms of Use',
+		component: () => <Terms />,
+	},
+];
+
+const hiddenPages = [
+	{
+		slug: '/style-guide',
+		title: 'Style Guide',
+		component: () => <StyleGuide />,
+	},
+];
+
+const clientPages = [
+	{
+		slug: '/wondai-country-festival',
+		title: 'Wondai Country Running Festival',
+		component: () => <WondaiCountry />,
+	},
+	{
+		slug: '/wondai-country-festival/socialpilot',
+		title: 'SocialPilot Instructions - Wondai Country Running Festival',
+		component: () => <SocialPilot />,
+	},
+	{
+		slug: '/glenrock-hay',
+		title: 'Glenrock Hay',
+		component: () => <GlenrockHay />,
+	},
+];
+
+const routeItems = menuItems
+	.concat(legalItems)
+	.concat(hiddenPages)
+	.concat(clientPages);
 
 class Meta extends Component {
-  render() {
-      return (
-          <Helmet>
-              <link rel="shortcut icon" href="/src/img/favicons/favicon.png" />
-              <link rel="icon" type="image/png" sizes="16x16" href="/src/img/favicons/favicon-16.png" />
-              <link rel="icon" type="image/png" sizes="32x32" href="/src/img/favicons/favicon-32.png" />
-              <link rel="icon" type="image/png" sizes="96x96" href="/src/img/favicons/favicon-96.png" />
+	render() {
+		return (
+			<Helmet>
+				<link
+					rel="shortcut icon"
+					href="/src/img/favicons/favicon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/src/img/favicons/favicon-16.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/src/img/favicons/favicon-32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="96x96"
+					href="/src/img/favicons/favicon-96.png"
+				/>
 
-              <link rel="apple-touch-icon" href="/src/img/favicons/favicon-180.png" />
-              <meta name="theme-color" content="#1C75BC" />
-              <link rel="icon" sizes="192x192" href="/src/img/favicons/favicon-192.png" />
-          </Helmet>
-      );
-  }
-};
-
+				<link
+					rel="apple-touch-icon"
+					href="/src/img/favicons/favicon-180.png"
+				/>
+				<meta name="theme-color" content="#1C75BC" />
+				<link
+					rel="icon"
+					sizes="192x192"
+					href="/src/img/favicons/favicon-192.png"
+				/>
+			</Helmet>
+		);
+	}
+}
 
 export class App extends Component {
-  render() {
-    window.onscroll = function() {
-      const perHeight = window.innerHeight * 0.3;
-      if(document.documentElement.scrollTop > perHeight) {
-          document.getElementById('root').classList.add('scrolled');
-      }
-      else {
-          document.getElementById('root').classList.remove('scrolled');
-      };
-    };
+	render() {
+		window.onscroll = function() {
+			const perHeight = window.innerHeight * 0.3;
+			if (document.documentElement.scrollTop > perHeight) {
+				document.getElementById('root').classList.add('scrolled');
+			} else {
+				document.getElementById('root').classList.remove('scrolled');
+			}
+		};
 
-    document.getElementById('root').classList.remove('style-guide');
+		document.getElementById('root').classList.remove('style-guide');
 
-    let pages = routeItems.map(page => {
-      return(
-        <div key={page.title}>
-          <Route exact path={page.slug} component={page.component} key={page.slug} />
-        </div>
-      );
-    });
+		let pages = routeItems.map((page) => {
+			return (
+				<Route
+					exact
+					path={page.slug}
+					component={page.component}
+					key={page.slug}
+				/>
+			);
+		});
 
-    return (
-        <Fragment>
-          <Meta />
-          <Router>
-            <div className="container">
-              {pages}
-            </div>
-          </Router>
-        </Fragment>
-    );
-  }
+		return (
+			<div className="container">
+				<Meta />
+				<Switch>{pages}</Switch>
+			</div>
+		);
+	}
 }
