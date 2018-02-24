3180706734c014bb61bd2c9dfa713a6bf6a89fae
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-02-24 08:11:51 +0800

    updated timeout for placeholder load

--- src/js/partial/scrollToTop.js
+++ src/js/partial/scrollToTop.js
@@ -2,6 +2,13 @@ import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 class ScrollToTop extends Component {
+	componentWillUpdate() {
+		let theseImages = document.getElementsByClassName('loaded');
+		for (let i=0; i < theseImages.length; i++) {
+			theseImages[i].classList.remove('loaded');
+		};
+	}
+
 	componentDidUpdate(prevProps) {
 		if (this.props.location !== prevProps.location) {
 			window.scrollTo(0, 0);
@@ -12,6 +19,14 @@ class ScrollToTop extends Component {
 				document.getElementById('nav-main').classList.remove('active');
 			}
 		}
+		
+		let theseImages = document.getElementsByClassName('placeholder');
+			setTimeout(function() {
+				for (let i=0; i < theseImages.length; i++) {
+					theseImages[i].classList.add('loaded');
+				};
+			}, 
+			1000);
 	}
 
 	render() {
