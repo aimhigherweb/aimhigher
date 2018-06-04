aa33f685c933986358e90c0d6f41bda1df1965c0
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-06-04 20:04:39 +0800

    It's working now, I swear!

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,9 +4,9 @@ import { Route, Switch } from 'react-router-dom';
 //Components
 import Home from '../../layouts/home/index.js';
 import StandardPage from '../../layouts/standard/index.js';
-import ProductsServices from '../../layouts/services/index.js';
+// import ProductsServices from '../../layouts/services/index.js';
 // import Portfolio from '../../layouts/portfolio/index.js';
-import Faq from '../../layouts/faq/index.js';
+// import Faq from '../../layouts/faq/index.js';
 import Feed from '../../layouts/blog/index.js';
 import Article from '../../layouts/blog/article/index.js';
 // import StyleGuide from '../../components/styleGuide/index.js';
@@ -22,25 +22,25 @@ export const menuItems = [
 		slug: '/about',
 		title: 'About',
   },
-  {
-		slug: '/services',
-		title: 'Services',
-		component: ProductsServices,
-  },
+  // {
+	// 	slug: '/services',
+	// 	title: 'Services',
+	// 	component: ProductsServices,
+  // },
   // {
 	// 	slug: '/portfolio',
 	// 	title: 'Portfolio',
 	// 	component: Portfolio,
   // },
-  {
-		slug: '/ethics',
-		title: 'Code of Ethics',
-  },
-  {
-		slug: '/faq',
-		title: 'FAQ',
-		component: Faq,
-  },
+  // {
+	// 	slug: '/ethics',
+	// 	title: 'Code of Ethics',
+  // },
+  // {
+	// 	slug: '/faq',
+	// 	title: 'FAQ',
+	// 	component: Faq,
+  // },
   {
 		slug: '/blog',
 		title: 'Blog',
@@ -54,10 +54,10 @@ export const menuItems = [
       }
     ]
   },
-  {
-		slug: '/contact',
-		title: 'Contact',
-  },
+  // {
+	// 	slug: '/contact',
+	// 	title: 'Contact',
+  // },
   // {
 	// 	slug: '/style-guide',
 	// 	title: 'Style Guide',
@@ -67,46 +67,54 @@ export const menuItems = [
 ];
 
 export const legalItems = [
-  {
-    slug: '/privacy',
-    title: 'Privacy Policy',
-  },
-  {
-    slug: '/terms',
-    title: 'Terms and Conditions',
-  }
+  // {
+  //   slug: '/privacy',
+  //   title: 'Privacy Policy',
+  // },
+  // {
+  //   slug: '/terms',
+  //   title: 'Terms and Conditions',
+  // }
 ];
 
 const routeItems = menuItems.concat(legalItems);
 
 class App extends Component {
   render() {
-    let pages = routeItems.map((page) => {
+    let subPages = [],
+        pages = routeItems.map((page) => {
       if(page.subPages) {
         let subp = page.subPages;
-        let subPages = subp.map((subpage) => {
+        let thisSubPages = subp.map((subpage) => {
           return (
             <Route path={page.slug + subpage.slug} exact component={subpage.component} key={subpage.slug} />
           );
         });
 
+        const sp = subPages;
+
+        subPages = sp.concat(thisSubPages);
+
         return (
-          <Fragment key={page.slug}>
-            <Route path={page.slug} exact component={page.component} />
-            {subPages}
-          </Fragment>
+            <Route key={page.slug} path={page.slug} exact component={page.component} />
         );
       }
       else if (page.component) {
-        return (
-          <Route path={page.slug} exact component={page.component} key={page.slug} />
-        );
-      };
+        if(page.default) {
+          return;
+        }
+        else {
+          return (
+            <Route path={page.slug} exact component={page.component} key={page.slug} />
+          );
+        };
+      }
     });
     return (        
       <Switch>
-            {pages}
-            <Route component={StandardPage} />
+          {pages}
+          {subPages}
+          <Route component={StandardPage} />
       </Switch>
     );
   }
