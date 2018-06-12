33fed2167c436e4c63724494e001c6491c6ae12f
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-06-12 16:46:33 +0800

    tweaked menu

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -21,16 +21,16 @@ export const Nav = styled('nav')`
 
 export const Item = styled(NavLink)`
     border-bottom: 3px solid transparent;
-    border-right: 2px solid ${props => props.theme.colours.primary.dark['90']};
+    border-right: 2px solid rgba(0, 0, 0, 0.3);
     color: #ffffff;
     font: ${props => props.theme.fonts.headings};
     font-size: 1.1em;
     font-weight: 500;
-    padding: 2px 10px;
+    padding: 0 10px;
     text-decoration: none;
 
     &:first-child {
-        border-left: 2px solid ${props => props.theme.colours.primary.dark['90']};
+        border-left: 2px solid rgba(0, 0, 0, 0.5);
     }
 
     &:active, &:hover, &:focus {
