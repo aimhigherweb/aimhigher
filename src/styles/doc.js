6e54d1da26a9c9acaba6be8c6808a53bd5c57395
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-05-27 11:25:29 +0800

    making docs narrower

--- src/styles/doc.js
+++ src/styles/doc.js
@@ -3,11 +3,13 @@ import styled from '@emotion/styled'
 import { Content } from '../components/layout/style'
 
 export const DocContent = styled(Content)`
+	max-width: 1000px;
+
 	.gatsby-resp-image-wrapper {
 		margin: 20px 0;
-	}
 
-	img {
-		max-height: unset;
+		img {
+			max-height: unset;
+		}
 	}
 `
