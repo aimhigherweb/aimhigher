14e87cb0187261c2e3cd93ba5103bf3efd706945
Amy Kapernick <amy@aimhigherweb.design>
2021-02-15 19:54:42 +0800

    updated module, and working on mobile nav

--- src/components/parts/nav/main/index.js
+++ src/components/parts/nav/main/index.js
@@ -1,22 +1,32 @@
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useState } from 'react';
 
+import Icon from '../../../../../lib/parts/icon';
 import styles from './main.module.scss';
 
-const Nav = ({ items }) => (
-	<nav className={styles.nav}>
-		<ul>
-			{items.map(({ label, link }) => (
-				<li key={label}>
-					<Link
-						to={link}
-						activeClassName="current"
-						className={styles.item}
-					>{label}</Link>
-				</li>
-			))}
-		</ul>
-	</nav>
-);
+const Nav = ({ items }) => {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<nav className={styles.nav} open={open}>
+			<button onClick={() => setOpen(!open)} className={styles.hamburger}>
+				<Icon icon="hamburger" className={styles.menu} />
+				<Icon icon="close" className={styles.close} />
+				<span className="sr-only">Toggle Main Nav</span>
+			</button>
+			<ul>
+				{items.map(({ label, link }) => (
+					<li key={label}>
+						<Link
+							to={link}
+							activeClassName="current"
+							className={styles.item}
+						>{label}</Link>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+};
 
 export default Nav;
