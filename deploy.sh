3c54523fc936bb555be0aded49738919503e0a7b
Amy Kapernick <amy@kapers.dev>
2022-10-27 16:19:28 +0800

    testing script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+echo 'Deploying works'
+echo $NETLIFY_SITE_ID
+
+
+# OUTPUT=$(sh -c netlify deploy --build --site $NETLIFY_SITE_ID)
+#           NETLIFY_OUTPUT=$(echo "$OUTPUT")
+#           NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
+#           NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
+#           NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+#           echo $NETLIFY_OUTPUT
+#           echo $NETLIFY_URL
+#           echo $NETLIFY_LOGS_URL
+#           echo $NETLIFY_LIVE_URL
