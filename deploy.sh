7a9c547a73a1c89de46449cf3806da54ad78c312
Amy Kapernick <amy@kapers.dev>
2022-10-27 17:17:46 +0800

    testing saving value

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,12 @@ NETLIFY_LOGS="${OUTPUT}" | jq -r '.logs'
 DEPLOY_ID="${OUTPUT}" | jq -r '.deploy_id'
 SITE_NAME="${OUTPUT}" | jq -r '.site_name'
 
+echo "${OUTPUT}" | jq -r '.deploy_url'
+
+echo 'Printing out items'
 echo $NETLIFY_URL
 echo $NETLIFY_LOGS
 echo $DEPLOY_ID
 echo $SITE_NAME
+
+echo "NETLIFY_URL=${NETLIFY_URL}" >> $GITHUB_OUTPUT
