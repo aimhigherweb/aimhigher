a0dfdb25d71fe391b016ffbcd7a188fd76708439
Amy Kapernick <amy@kapers.dev>
2022-10-27 17:57:34 +0800

    getting all values

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 
 OUTPUT=$(netlify deploy --build --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --json --message "Deploying from GitHub Actions")
 
-NETLIFY_URL=jq -r '.deploy_url' <<<"$OUTPUT"
-NETLIFY_LOGS="${OUTPUT}" | jq -r '.logs' <<<$OUTPUT
+NETLIFY_URL=$(jq -r '.deploy_url' <<<"${OUTPUT}")
+NETLIFY_LOGS=$(jq -r '.logs' <<<"${OUTPUT}")
 DEPLOY_ID=$(jq -r '.deploy_id' <<<"${OUTPUT}")
-SITE_NAME="${OUTPUT}" | jq -r '.site_name'
+SITE_NAME=$(jq -r '.site_name' <<<"${OUTPUT}")
 
 echo "${OUTPUT}"
 echo "${OUTPUT}" | jq -r '.deploy_url'
