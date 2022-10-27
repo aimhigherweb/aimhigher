30ddf39901a39b86acbf9546fbac558fdb7951dd
Amy Kapernick <amy@kapers.dev>
2022-10-27 17:31:58 +0800

    testing otuput

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 
 OUTPUT=$(netlify deploy --build --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --json --message "Deploying from GitHub Actions")
 
-NETLIFY_URL="${OUTPUT}" | jq -r '.deploy_url'
-NETLIFY_LOGS="${OUTPUT}" | jq -r '.logs'
+NETLIFY_URL=$("${OUTPUT}" | jq -r '.deploy_url')
+NETLIFY_LOGS=$("${OUTPUT}" | jq -r '.logs')
 DEPLOY_ID="${OUTPUT}" | jq -r '.deploy_id'
 SITE_NAME="${OUTPUT}" | jq -r '.site_name'
 
-echo ${OUTPUT}
+echo "${OUTPUT}"
 echo "${OUTPUT}" | jq -r '.deploy_url'
 
 echo 'Printing out items'
